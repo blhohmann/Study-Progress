58822fcb6127d5eae60920a83609b9bdbfa17165
blhohmann <blake-hohmann@uiowa.edu>
2024-08-24 00:48:26 -0500

    feat: add fun mascot guy

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<!-- <template>
-  <div></div>
-</template> -->
-
 <template>
-  <div id="app">
-    <ChartComponent />
-  </div>
+  <VApp class="rounded rounded-md">
+    <!-- Sidebar -->
+
+    <!-- Cards -->
+    <v-app-bar>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="text-center">What, like it's hard?</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <div id="app">
+        <ChartComponent />
+      </div>
+      <!-- this is only for personal use cannot use commercially, generated via https://designer.microsoft.com/image-creator -->
+      <v-img src="@/assets/happy.jpeg" aspect-ratio="2.75"></v-img>
+    </v-main>
+  </VApp>
 </template>
 
 <script>
